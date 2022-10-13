25212c0f349d5b843107e3b77f06549ebb4cffef
Thiago Trancoso <thiagostrn@gmail.com>
2022-10-13 18:19:28 -0300

    Módulo 2 - Aula 6

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-criar-pensamento',
+    templateUrl: './criar-pensamento.component.html',
+    styleUrls: ['./criar-pensamento.component.scss']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+    pensamento = {
+        id: '1',
+        conteudo: 'Aprendendo Angular',
+        autoria: 'Dev',
+        modelo: 'modelo2'
+    }
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    criarPensamento() {
+        alert('Novo pensamento criado!')
+    }
+
+    cancelar() {
+
+    }
+}
