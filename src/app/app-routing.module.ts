17e756944c69479077a2546e3fbc5d5e168ce7f6
Thiago Trancoso <thiagostrn@gmail.com>
2022-10-14 08:18:07 -0300

    Módulo 3 - Aula 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'listar-pensamento'
+    },
+    {
+        path: 'criar-pensamento',
+        component: CriarPensamentoComponent
+    },
+    {
+        path: 'listar-pensamento',
+        component: ListarPensamentoComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
