86dbdbd0d06d297202748992d2ef8e31c6460b8e
Thiago Trancoso <thiagostrn@gmail.com>
2022-10-16 11:25:39 -0300

    Módulo 5 - Aula 5

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PensamentoService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
 }
